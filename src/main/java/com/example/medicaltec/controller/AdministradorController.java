75a2616e15efb5805d027634980e987cd95a328d
xTarantino28 <gzanabriaz28@gmail.com>
2023-04-09 17:50:16 -0500

    administrador controller y carpeta administrador

---
+++ src/main/java/com/example/medicaltec/controller/AdministradorController.java
@@ -0,0 +1,43 @@
+package com.example.medicaltec.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+@RequestMapping("/administrador")
+public class AdministradorController {
+
+    @RequestMapping("/principal")
+    public String pagprincipal(){
+
+        return "administrador/paginaprincipal";
+    }
+
+    @RequestMapping("/usuarios")
+    public String pagusuarios(){
+
+        return "administrador/usuarios";
+    }
+
+    @RequestMapping("/calendario")
+    public String calendario(){
+
+        return "administrador/calendario";
+    }
+
+    @RequestMapping("/finanzas")
+    public String finanzas(){
+
+        return "administrador/finanzas";
+    }
+
+    @RequestMapping("/settings")
+    public String settings(){
+
+        return "administrador/settings";
+    }
+
+
+
+}
