725ec2541acd5d79d54fe64d9eb6167ffe62c7ee
Leonardo <a20201696@pucp.edu.pe>
2023-04-21 23:35:25 -0500

    a

---
+++ src/main/java/com/example/medicaltec/MedicaltecApplication.java
@@ -0,0 +1,13 @@
+package com.example.medicaltec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicaltecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicaltecApplication.class, args);
+    }
+
+}
