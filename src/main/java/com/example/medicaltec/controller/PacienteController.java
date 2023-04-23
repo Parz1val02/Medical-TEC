daa1c924aa6d615ca19cd1812967f940fd6bcb14
unknown <luis.ramost@pucp.edu.pe>
2023-04-22 22:25:39 -0500

    cambio de contraseña en otra pagina y modal de receta

--- src/main/java/com/example/medicaltec/controller/PacienteController.java
+++ src/main/java/com/example/medicaltec/controller/PacienteController.java
@@ -2,20 +2,37 @@ package com.example.medicaltec.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/paciente")
 public class PacienteController {
 
-   @RequestMapping(value = "/principal")
-    public String paginaprincipal(){
+    /*final SedeRepository sedeRepository;
+
+    /*public PacienteController(SedeRepository sedeRepository) {
+        this.sedeRepository = sedeRepository;
+    }*/
+
+    @RequestMapping(value = "/principal")
+    public String paginaprincipal(Model model){
+
+       //model.addAttribute("sedes",sedeRepository.findAll());
        return "paciente/principal";
    }
 
     @RequestMapping("/perfil")
-    public String perfilpaciente(){
+    public String perfilpaciente(Model model){
+
         return "paciente/perfil";
+    }
+
+    @GetMapping("/password")
+    public String cambiarContra(Model model){
+
+        return "paciente/cambioContrasena";
     }
 
     @RequestMapping("/consultas")
