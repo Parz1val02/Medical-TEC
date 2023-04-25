fab11507ac6486d7deb2bf01e475f6cec4180ec6
r.b4rr10s <a20202073@pucp.edu.pe>
2023-04-25 15:40:38 -0500

    Avances a sede de paciente y lista de doctores

---
+++ src/main/java/com/example/medicaltec/entity/Tarjeta.java
@@ -0,0 +1,2 @@
+package com.example.medicaltec.entity;public class Tarjeta {
+}
---
+++ src/main/java/com/example/medicaltec/repository/TipoCitaRepository.java
@@ -0,0 +1,6 @@
+package com.example.medicaltec.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipoCitaRepository extends JpaRepository<TipoCita, Integer> {
+}
