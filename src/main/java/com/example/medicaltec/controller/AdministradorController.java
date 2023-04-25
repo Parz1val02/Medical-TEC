c3b975294d74aca1351b4742a60a06319bd2ac85
Gil Zanabria <gzanabriaz28@gmail.com>
2023-04-25 15:44:36 -0500

    se puede editar doctor en usuarios

--- src/main/java/com/example/medicaltec/controller/AdministradorController.java
+++ src/main/java/com/example/medicaltec/controller/AdministradorController.java
@@ -1,10 +1,16 @@
 package com.example.medicaltec.controller;
+import com.example.medicaltec.entity.Especialidade;
+import com.example.medicaltec.entity.Usuario;
+import com.example.medicaltec.repository.EspecialidadeRepository;
 import com.example.medicaltec.repository.ReporteRepository;
 import com.example.medicaltec.repository.UsuarioRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -23,12 +29,13 @@ public class AdministradorController {
      */
 
 
-
+    final EspecialidadeRepository especialidadeRepository;
     final UsuarioRepository usuarioRepository;
     final ReporteRepository reporteRepository;
-    public AdministradorController (UsuarioRepository usuarioRepository, ReporteRepository reporteRepository) {
+    public AdministradorController (UsuarioRepository usuarioRepository, ReporteRepository reporteRepository,EspecialidadeRepository especialidadeRepository) {
         this.usuarioRepository = usuarioRepository;
         this.reporteRepository = reporteRepository;
+        this.especialidadeRepository = especialidadeRepository;
     }
 
 
@@ -44,12 +51,37 @@ public class AdministradorController {
 
     @GetMapping("/usuarios")
     public String pagusuarios(Model model){
-        List listaPacientes = usuarioRepository.obtenerListaPacientes();
-        List listaDoctores = usuarioRepository.obtenerlistaDoctores();
+        List<Especialidade> listaEspecialidades = especialidadeRepository.findAll();
+        List<Usuario> listaPacientes = usuarioRepository.obtenerListaPacientes();
+        List<Usuario> listaDoctores = usuarioRepository.obtenerlistaDoctores();
+        model.addAttribute("listaEspecialidades",listaEspecialidades);
         model.addAttribute("listaPacientes",listaPacientes);
         model.addAttribute("listaDoctores",listaDoctores);
         return "administrador/usuarios";
     }
+
+    @PostMapping("/editarDoctor")
+    public String editarDoctor(
+            @RequestParam("sede") int sede,
+            @RequestParam("nombre") String nombre,
+            @RequestParam("email") String email,
+            @RequestParam("id") String id,
+            @RequestParam("apellido") String apellido,
+            @RequestParam("especialidad") int especialidad,
+            @RequestParam("telefono") String telefono,
+            RedirectAttributes attr
+
+    ){
+        System.out.println(nombre);
+        attr.addFlashAttribute("msg","Doctor actualizado exitosamente");
+        usuarioRepository.editarDoctor( email,  nombre,  apellido,  telefono,  especialidad,  id,  sede );
+        return "redirect:/administrador/usuarios";
+    }
+
+
+
+
+
 
     @GetMapping("/calendario")
     public String calendario(){
--- src/main/java/com/example/medicaltec/repository/UsuarioRepository.java
+++ src/main/java/com/example/medicaltec/repository/UsuarioRepository.java
@@ -1,7 +1,9 @@
 package com.example.medicaltec.repository;
 
 import com.example.medicaltec.entity.Usuario;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.lang.NonNull;
 
@@ -19,5 +21,9 @@ public interface UsuarioRepository extends JpaRepository<Usuario, String> {
     @Query(nativeQuery = true,value = "select * from Usuario u where u.roles_idroles = 1 and u.sedes_idsedes = 1")
     List<Usuario> obtenerlistaDoctores();
 
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true,value = "update usuario u set u.email=?1, u.nombre=?2, u.apellido=?3, u.telefono=?4, u.especialidades_id_especialidad=?5 where  u.dni = ?6 and u.sedes_idsedes = ?7")
+    void editarDoctor(String email, String nombre, String apellido, String telefono, int especialidad, String id, int sede );
 
 }
