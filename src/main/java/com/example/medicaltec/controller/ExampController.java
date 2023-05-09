4cf85c432de9abd4246de06533d2b174d5f9e493
r.b4rr10s <a20202073@pucp.edu.pe>
2023-05-09 15:26:10 -0500

    Login

--- src/main/java/com/example/medicaltec/controller/ExampController.java
+++ src/main/java/com/example/medicaltec/controller/ExampController.java
@@ -1,16 +1,59 @@
 package com.example.medicaltec.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+        import com.example.medicaltec.Entity.Usuario;
+        import com.example.medicaltec.repository.UsuarioRepository;
+        import org.springframework.stereotype.Controller;
+        import org.springframework.ui.Model;
+        import org.springframework.web.bind.annotation.PostMapping;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RequestMethod;
+        import org.springframework.web.bind.annotation.RequestParam;
+
+        import java.util.List;
 
 @Controller
 public class ExampController {
+    final UsuarioRepository usuarioRepository;
+
+    public ExampController(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
 
     @RequestMapping(value = {"/"},method = RequestMethod.GET)
     public String login(){
         return "auth/login";
     }
 
-
+    @PostMapping(value = "/login")
+    public String pagPrincipalDoctor(@RequestParam("email") String email,
+                                     @RequestParam("password") String password,
+                                     Model model){
+        boolean i=false;
+        Usuario user1 = new Usuario();
+        List<Usuario> userList = usuarioRepository.findAll();
+        for(Usuario u : userList){
+            if(u.getEmail().equals(email) && u.getContrasena().equals(password)) {
+                i=true;
+                user1 = u;
+                break;
+            }
+        }
+        if(i){
+            switch(user1.getRolesIdroles().getNombreRol()){
+                case "paciente":
+                    return "redirect:/paciente/principal";
+                case "administrativo":
+                    return "redirect:/administrativo/dashboard";
+                case "administrador":
+                    return "redirect:/administrador/principal";
+                case "superadmin":
+                    return "redirect:/superadmin/dashboard";
+                default:
+                    model.addAttribute("user", user1);
+                    return "doctor/principal";
+            }
+        }else {
+            return "redirect:/";
+        }
+    }
 }
