c25ac123a1eda612177abfd57286bbc2c8ac83b8
leonardo <a20201696@pucp.edu.pe>
2023-05-09 15:34:23 -0500

    Cambios chiquis

--- src/main/java/com/example/medicaltec/controller/SuperController.java
+++ src/main/java/com/example/medicaltec/controller/SuperController.java
@@ -181,7 +181,7 @@ public class SuperController {
     }
     @PostMapping("/actualizarAdmS")
     public String actualizarAdministrador(@ModelAttribute("admS") Usuario admS, RedirectAttributes attr) {
-        attr.addFlashAttribute("msg", "Administrador actualizado exitosamente");
+        /*
         int a = 0;
         if(admS.getNombre().isEmpty()){
             attr.addFlashAttribute("nombremsg","El nombre no puede ser nulo");
@@ -195,19 +195,6 @@ public class SuperController {
             attr.addFlashAttribute("correomsg","El correo no puede ser nulo");
             a = a+1;
         }
-        if (admS.getContrasena().isEmpty()){
-            attr.addFlashAttribute("passwordmsg","La contraseña no puede ser nula");
-            a = a+1;
-        }
-        if (admS.getEdad() == null){
-            attr.addFlashAttribute("edadmsg","La edad no puede ser nula");
-            a = a+1;
-        } else {
-            if (admS.getEdad() <0){
-                attr.addFlashAttribute("edadmsg","La edad no puede ser negativa");
-                a = a+1;
-            }
-        }
         if (admS.getTelefono().isEmpty()){
             attr.addFlashAttribute("telefonomsg","El teléfono no puede ser nulo");
             a = a+1;
@@ -216,32 +203,17 @@ public class SuperController {
             attr.addFlashAttribute("telefonomsg", "El número de teléfono debe tener 9 dígitos");
             a = a+1;
         }
-        if(admS.getDireccion().isEmpty()) {
-            attr.addFlashAttribute("addressmsg","La dirección no puede ser nula");
-            a = a+1;
-        }
-        if(admS.getId().isEmpty()){
-            attr.addFlashAttribute("dnimsg","El DNI no puede ser nulo");
-            a = a+1;
-        } else if (admS.getId().length()!=8) {
-            attr.addFlashAttribute("dnimsg","El DNI tiene que tener 8 dígitos");
-            a = a+1;
-        } else {
-            Optional<Usuario> u = usuarioRepository.findById(admS.getId());
-            if(u.isPresent()){
-                attr.addFlashAttribute("dnimsg","El DNI ya se encuentra registrado.");
-                a = a+1;
-            }
-        }
-
         if(a == 0){
             usuarioRepository.editarAdministradores(admS.getEmail(), admS.getNombre(), admS.getApellido(), admS.getSedesIdsedes().getId(), admS.getTelefono(), admS.getEstadosIdestado().getId(), admS.getId());
             attr.addFlashAttribute("msg","Administrador de Sede creado exitosamente");
             return "redirect:/superAdmin/dashboard/Adm";
         }else {
             attr.addFlashAttribute("msg1","Hubieron errores en el llenado de los campos");
-            return "redirect:/superAdmin/editarAdmSede";
-        }
+            return "redirect:/superAdmin/editarAdmS";
+        } */
+        usuarioRepository.editarAdministradores(admS.getEmail(), admS.getNombre(), admS.getApellido(), admS.getSedesIdsedes().getId(), admS.getTelefono(), admS.getEstadosIdestado().getId(), admS.getId());
+        attr.addFlashAttribute("msg", "Administrador actualizado exitosamente");
+        return "redirect:/superAdmin/dashboard/Adm";
     }
     @GetMapping("/editarAdmT")
     public String editarAdministrativo(Model model, @ModelAttribute("admT") Usuario admT, @RequestParam("id") String dni, RedirectAttributes attr) {
