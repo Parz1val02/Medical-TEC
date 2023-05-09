117be5524f2adb32e77073dff447b5a49c5726aa
xTarantino28 <gzanabriaz28@gmail.com>
2023-05-09 16:27:57 -0500

    validaciones en pagina de usuarios

--- src/main/java/com/example/medicaltec/controller/AdministradorController.java
+++ src/main/java/com/example/medicaltec/controller/AdministradorController.java
@@ -157,14 +157,112 @@ public class AdministradorController {
             return "redirect:/administrador/usuarios";
         }
     }*/
+
+
+    @GetMapping("/crearDoctorPagina")
+    public String crearDoctorPagina(Model model, @ModelAttribute("doctor") Usuario doctor, RedirectAttributes attr){
+        List<Especialidade> listaEspecialidades = especialidadeRepository.findAll();
+        model.addAttribute("listaEspecialidades",listaEspecialidades);
+        return "administrador/crearDoctor";
+    }
+
     @PostMapping("/crearDoctor")
     public String crearDoctor(
-            @ModelAttribute("usuario") Usuario doctor,
-            RedirectAttributes attr
+            @ModelAttribute("doctor") @Valid Usuario doctor,
+            BindingResult bindingResult,
+            RedirectAttributes attr,
+            Model model
 
     ){
-        List<Usuario> listaDoctores = usuarioRepository.obtenerlistaDoctores();
 
+        /*
+        int a = 0;
+        if(doctor.getNombre().isEmpty()){
+            attr.addFlashAttribute("nombremsg","El nombre no puede ser nulo");
+            a = a+1;
+        }
+        if(doctor.getApellido().isEmpty()){
+            attr.addFlashAttribute("apellidomsg","El apellido no puede ser nulo");
+            a = a+1;
+        }
+        if (doctor.getEmail().isEmpty()){
+            attr.addFlashAttribute("correomsg","El correo no puede ser nulo");
+            a = a+1;
+        }
+        if (doctor.getEdad() == null){
+            attr.addFlashAttribute("edadmsg","La edad no puede ser nula");
+            a = a+1;
+        } else {
+            if (doctor.getEdad()  <0){
+                attr.addFlashAttribute("edadmsg","La edad no puede ser negativa");
+                a = a+1;
+            }
+        }
+        if (doctor.getTelefono().isEmpty()){
+            attr.addFlashAttribute("telefonomsg","El teléfono no puede ser nulo");
+            a = a+1;
+        }
+        if (doctor.getTelefono().length()!=9){
+            attr.addFlashAttribute("telefonomsg", "El número de teléfono debe tener 9 dígitos");
+            a = a+1;
+        }
+        if(doctor.getDireccion().isEmpty()) {
+            attr.addFlashAttribute("addressmsg","La dirección no puede ser nula");
+            a = a+1;
+        }
+        if(doctor.getId().isEmpty()){
+            attr.addFlashAttribute("dnimsg","El DNI no puede ser nulo");
+            a = a+1;
+        } else if (doctor.getId().length()!=8) {
+            attr.addFlashAttribute("dnimsg","El DNI tiene que tener 8 dígitos");
+            a = a+1;
+        } else {
+            Optional<Usuario> u = usuarioRepository.findById(doctor.getId());
+            if(u.isPresent()){
+                attr.addFlashAttribute("dnimsg","El DNI ya se encuentra registrado.");
+                a = a+1;
+            }
+        }
+        if(a == 0){
+            GeneradorDeContrasenha generadorDeContrasenha=new GeneradorDeContrasenha();
+            String contrasena = generadorDeContrasenha.crearPassword();
+            usuarioRepository.crearDoctor( paciente.getEmail(),  paciente.getNombre(),  paciente.getApellido(),  paciente.getTelefono(), paciente.getId(),  paciente.getSedesIdsedes().getId(), paciente.getEdad(), paciente.getDireccion() , paciente.getSexo(), contrasena );
+            attr.addFlashAttribute("msg","doctor creado exitosamente");
+            return "redirect:/administrador/usuarios";
+        }else {
+            attr.addFlashAttribute("msg1","Hubieron errores en el llenado de los campos");
+            return "administrador/crearPaciente";
+        }*/
+        int a = 0;
+        if (doctor.getEdad()!=null){
+            if (doctor.getEdad()<0) {
+                model.addAttribute("edadmsg","La edad debe ser un numero entero positivo");
+                a = a+1;
+            }
+
+
+        }
+
+        try {
+            int dniEntero = Integer.parseInt(doctor.getId());
+            if (dniEntero<0){
+                model.addAttribute("dnimsg","Deber ser un numero de DNI valido de maximo 8 digitos");
+            }
+        } catch (NumberFormatException e) {
+            model.addAttribute("dnimsg","Deber ser un numero de DNI valido de maximo 8 digitos");
+            a = a + 1;
+        }
+
+
+        try {
+            int telefonoEntero = Integer.parseInt(doctor.getTelefono());
+        } catch (NumberFormatException e) {
+            model.addAttribute("telefonomsg","El telefono debe ser un numero entero de máximo 9 digitos");
+            a = a + 1;
+        }
+
+
+        List<Usuario> listaDoctores = usuarioRepository.obtenerlistaDoctores();
         boolean existeDoctor = false;
         for (Usuario doctorLista : listaDoctores) {
             if (doctor.getId().equalsIgnoreCase(doctorLista.getId()) ) {
@@ -172,10 +270,15 @@ public class AdministradorController {
             }
         }
 
-
-        if ( existeDoctor) {
+        if ( existeDoctor && bindingResult.hasErrors()) {
             attr.addFlashAttribute("msgDanger","El doctor ingresado ya existe");
-            return "redirect:/administrador/usuarios";
+            List<Especialidade> listaEspecialidades = especialidadeRepository.findAll();
+            model.addAttribute("listaEspecialidades",listaEspecialidades);
+            return "administrador/crearDoctor";
+        } else if (bindingResult.hasErrors()) {
+            List<Especialidade> listaEspecialidades = especialidadeRepository.findAll();
+            model.addAttribute("listaEspecialidades",listaEspecialidades);
+            return "administrador/crearDoctor";
         } else {
             GeneradorDeContrasenha generadorDeContrasenha=new GeneradorDeContrasenha();
             String contrasena = generadorDeContrasenha.crearPassword();
@@ -274,12 +377,103 @@ public class AdministradorController {
     }
     */
 
+    @GetMapping("/crearPacientePagina")
+    public String crearPacientePagina(Model model, @ModelAttribute("paciente") Usuario paciente, RedirectAttributes attr){
+        return "administrador/crearPaciente";
+    }
+
     @PostMapping("/crearPaciente")
     public String crearPaciente(
-            @ModelAttribute("usuario") Usuario paciente,
-            RedirectAttributes attr
+            @ModelAttribute("paciente") @Valid Usuario paciente,
+            BindingResult bindingResult,
+            RedirectAttributes attr,
+            Model model
 
     ){
+        /*
+        int a = 0;
+        if(paciente.getNombre().isEmpty()){
+            attr.addFlashAttribute("nombremsg","El nombre no puede ser nulo");
+            a = a+1;
+        }
+        if(paciente.getApellido().isEmpty()){
+            attr.addFlashAttribute("apellidomsg","El apellido no puede ser nulo");
+            a = a+1;
+        }
+        if (paciente.getEmail().isEmpty()){
+            attr.addFlashAttribute("correomsg","El correo no puede ser nulo");
+            a = a+1;
+        }
+        if (paciente.getEdad() == null){
+            attr.addFlashAttribute("edadmsg","La edad no puede ser nula");
+            a = a+1;
+        } else {
+            if (paciente.getEdad()  <0){
+                attr.addFlashAttribute("edadmsg","La edad no puede ser negativa");
+                a = a+1;
+            }
+        }
+        if (paciente.getTelefono().isEmpty()){
+            attr.addFlashAttribute("telefonomsg","El teléfono no puede ser nulo");
+            a = a+1;
+        }
+        if (paciente.getTelefono().length()!=9){
+            attr.addFlashAttribute("telefonomsg", "El número de teléfono debe tener 9 dígitos");
+            a = a+1;
+        }
+        if(paciente.getDireccion().isEmpty()) {
+            attr.addFlashAttribute("addressmsg","La dirección no puede ser nula");
+            a = a+1;
+        }
+        if(paciente.getId().isEmpty()){
+            attr.addFlashAttribute("dnimsg","El DNI no puede ser nulo");
+            a = a+1;
+        } else if (paciente.getId().length()!=8) {
+            attr.addFlashAttribute("dnimsg","El DNI tiene que tener 8 dígitos");
+            a = a+1;
+        } else {
+            Optional<Usuario> u = usuarioRepository.findById(paciente.getId());
+            if(u.isPresent()){
+                attr.addFlashAttribute("dnimsg","El DNI ya se encuentra registrado.");
+                a = a+1;
+            }
+        }
+        if(a == 0){
+            GeneradorDeContrasenha generadorDeContrasenha=new GeneradorDeContrasenha();
+            String contrasena = generadorDeContrasenha.crearPassword();
+            usuarioRepository.crearPaciente( paciente.getEmail(),  paciente.getNombre(),  paciente.getApellido(),  paciente.getTelefono(), paciente.getId(),  paciente.getSedesIdsedes().getId(), paciente.getEdad(), paciente.getDireccion() , paciente.getSexo(), contrasena );
+            attr.addFlashAttribute("msg","Paciente creado exitosamente");
+            return "redirect:/administrador/usuarios";
+        }else {
+            attr.addFlashAttribute("msg1","Hubieron errores en el llenado de los campos");
+            return "administrador/crearPaciente";
+        }*/
+        int a = 0;
+        if (paciente.getEdad()!=null){
+           if (paciente.getEdad()<0) {
+               model.addAttribute("edadmsg","La edad debe ser un numero entero positivo");
+               a = a+1;
+           }
+
+
+        }
+        try {
+            int dniEntero = Integer.parseInt(paciente.getId());
+            if (dniEntero<0){
+                model.addAttribute("dnimsg","Deber ser un numero de DNI valido de maximo 8 digitos");
+            }
+        } catch (NumberFormatException e) {
+            model.addAttribute("dnimsg","Deber ser un numero de DNI valido de maximo 8 digitos");
+            a = a + 1;
+        }
+        try {
+            int telefonoEntero = Integer.parseInt(paciente.getTelefono());
+        } catch (NumberFormatException e) {
+            model.addAttribute("telefonomsg","El telefono debe ser un numero entero de máximo 9 digitos");
+            a = a + 1;
+        }
+
+
         List<Usuario> listaPacientes = usuarioRepository.obtenerListaPacientes();
         boolean existePaciente = false;
         for (Usuario pacienteLista : listaPacientes) {
@@ -288,10 +482,11 @@ public class AdministradorController {
             }
         }
 
-
-        if ( existePaciente) {
+        if ( existePaciente && bindingResult.hasErrors()) {
             attr.addFlashAttribute("msgDanger","El paciente ingresado ya existe");
-            return "redirect:/administrador/usuarios";
+            return "administrador/crearPaciente";
+        } else if (bindingResult.hasErrors()) {
+            return "administrador/crearPaciente";
         } else {
             GeneradorDeContrasenha generadorDeContrasenha=new GeneradorDeContrasenha();
             String contrasena = generadorDeContrasenha.crearPassword();
@@ -299,6 +494,7 @@ public class AdministradorController {
             attr.addFlashAttribute("msg","Paciente creado exitosamente");
             return "redirect:/administrador/usuarios";
         }
+
     }
 
 
