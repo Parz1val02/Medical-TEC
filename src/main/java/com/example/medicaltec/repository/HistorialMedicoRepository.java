bb9c211a318ad69ebd0b22807da9389c6d948153
unknown <luis.ramost@pucp.edu.pe>
2023-05-20 20:08:00 -0500

    app

--- src/main/java/com/example/medicaltec/repository/HistorialMedicoRepository.java
+++ src/main/java/com/example/medicaltec/repository/HistorialMedicoRepository.java
@@ -2,6 +2,9 @@ package com.example.medicaltec.repository;
 
 import com.example.medicaltec.Entity.Historialmedico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface HistorialmedicoRepository extends JpaRepository<Historialmedico, Integer> {
+public interface HistorialMedicoRepository extends JpaRepository<Historialmedico, Integer> {
+    @Query(nativeQuery = true, value = "SELECT idhistorialmedico FROM telesystem.historialmedico where idhistorialmedico=?1")
+    String verificaridHistorialMedico(String id);
 }
