5b927e2493e66f15d48e95d2807aa007651c4cf6
unknown <luis.ramost@pucp.edu.pe>
2023-05-21 22:39:20 -0500

    gil arregla esto

--- src/main/java/com/example/medicaltec/Entity/Estado.java
+++ src/main/java/com/example/medicaltec/Entity/Estado.java
@@ -4,11 +4,13 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @Entity
 @Table(name = "estados")
-public class Estado {
+public class Estado implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idestado", nullable = false)
