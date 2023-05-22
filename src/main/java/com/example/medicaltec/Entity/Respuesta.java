65c4f64292c7815cd3b40a0d4f523f6202f1c73a
unknown <luis.ramost@pucp.edu.pe>
2023-05-22 13:52:29 -0500

    cerrar sesion en todos los flujos

--- src/main/java/com/example/medicaltec/Entity/Respuesta.java
+++ src/main/java/com/example/medicaltec/Entity/Respuesta.java
@@ -5,11 +5,13 @@ import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @Entity
 @Table(name = "respuestas")
-public class Respuesta {
+public class Respuesta implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idrespuestas", nullable = false)
