8423623e4695ced4692d8c644772d68930bed496
leonardo <a20201696@pucp.edu.pe>
2023-05-30 17:09:01 -0500

    dump

---
+++ telesystem_2/telesystem_2_cuestionario.sql
@@ -0,0 +1,64 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for macos12 (x86_64)
+--
+-- Host: 35.231.147.174    Database: telesystem_2
+-- ------------------------------------------------------
+-- Server version	8.0.26-google
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '';
+
+--
+-- Table structure for table `cuestionario`
+--
+
+DROP TABLE IF EXISTS `cuestionario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cuestionario` (
+  `idcuestionario` int NOT NULL AUTO_INCREMENT,
+  `nombrecuestionario` varchar(45) NOT NULL,
+  `activo` tinyint NOT NULL,
+  `paciente_dni` varchar(8) NOT NULL,
+  PRIMARY KEY (`idcuestionario`),
+  KEY `fk_cuestionario_usuario1_idx` (`paciente_dni`),
+  CONSTRAINT `fk_cuestionario_usuario1` FOREIGN KEY (`paciente_dni`) REFERENCES `usuario` (`dni`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cuestionario`
+--
+
+LOCK TABLES `cuestionario` WRITE;
+/*!40000 ALTER TABLE `cuestionario` DISABLE KEYS */;
+INSERT INTO `cuestionario` VALUES (1,'Cuestionario 1',1,'22647853'),(2,'Cuestionario 2',1,'23456789'),(3,'Cuestionario 3',1,'23766788'),(4,'Cuestionario 4',1,'28573467'),(5,'Cuestionario 5',1,'45678975'),(6,'Cuestionario general',0,'22647853');
+/*!40000 ALTER TABLE `cuestionario` ENABLE KEYS */;
+UNLOCK TABLES;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-30 16:22:28
---
+++ telesystem_2/telesystem_2_respuestas.sql
@@ -0,0 +1,66 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for macos12 (x86_64)
+--
+-- Host: 35.231.147.174    Database: telesystem_2
+-- ------------------------------------------------------
+-- Server version	8.0.26-google
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '';
+
+--
+-- Table structure for table `respuestas`
+--
+
+DROP TABLE IF EXISTS `respuestas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `respuestas` (
+  `idrespuestas` int NOT NULL AUTO_INCREMENT,
+  `respuesta` varchar(200) NOT NULL,
+  `preguntas_idpreguntas` int NOT NULL,
+  `historialmedico_idhistorialmedico` int NOT NULL,
+  PRIMARY KEY (`idrespuestas`),
+  KEY `fk_respuestas_preguntas1_idx` (`preguntas_idpreguntas`),
+  KEY `fk_respuestas_historialmedico1_idx` (`historialmedico_idhistorialmedico`),
+  CONSTRAINT `fk_respuestas_historialmedico1` FOREIGN KEY (`historialmedico_idhistorialmedico`) REFERENCES `historialmedico` (`idhistorialmedico`),
+  CONSTRAINT `fk_respuestas_preguntas1` FOREIGN KEY (`preguntas_idpreguntas`) REFERENCES `preguntas` (`idpreguntas`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `respuestas`
+--
+
+LOCK TABLES `respuestas` WRITE;
+/*!40000 ALTER TABLE `respuestas` DISABLE KEYS */;
+INSERT INTO `respuestas` VALUES (1,'no',1,1),(2,'',1,1);
+/*!40000 ALTER TABLE `respuestas` ENABLE KEYS */;
+UNLOCK TABLES;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-30 16:22:50
