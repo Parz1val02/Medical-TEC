75129d6c2a613cbc478ed30eda82794f20844995
xTarantino28 <gzanabriaz28@gmail.com>
2023-06-04 14:35:29 -0500

    administrador acepta lista formularios de registro para invitar masivamente y crearlos en BD

--- src/main/java/com/example/medicaltec/repository/FormInvitationRepository.java
+++ src/main/java/com/example/medicaltec/repository/FormInvitationRepository.java
@@ -2,11 +2,28 @@ package com.example.medicaltec.repository;
 
 import com.example.medicaltec.Entity.FormInvitacion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface FormInvitationRepository extends JpaRepository<FormInvitacion,Integer> {
 
     @Query(nativeQuery = true,value="select * from form_invitacion where dni = ?")
     FormInvitacion findFormbyPacient(String dni);
 
+
+    /*QUERYS USADOOS POR ADMINISTRADOR*/
+    @Query(nativeQuery = true,value="select * from form_invitacion where id_sede = ?1 and pendiente = 1")
+    List<FormInvitacion> findFormbySede(Integer sede);
+
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true,value = "UPDATE form_invitacion SET pendiente = 0 WHERE idform_invitacion = ?1 ")
+    void actualizarEstadoFormRegistroRevisado(Integer idform_invitacion);
+    /*FIN QUERYS USADOOS POR ADMINISTRADOR*/
+    /* ************************************ */
+    /* ************************************ */
 }
