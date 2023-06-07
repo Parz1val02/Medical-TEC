c716a6b5abc9bc86dd75db2b56ea19b906ee06c3
Luis Ramos <108200512+lucho2000@users.noreply.github.com>
2023-06-06 20:47:51 -0500

    Update app.js

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,2 +1,7 @@
+function getCurrentURL () {
+  return window.location.href
+}
+// Example
+const url = getCurrentURL()
 const QRcontenedor = document.getElementById('contenedorQR')
-new QRCode(QRcontenedor, "")
+new QRCode(QRcontenedor, url)
