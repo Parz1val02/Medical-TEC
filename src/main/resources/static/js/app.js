03d84438ca32102b7c4920dc156039a629ad0995
lucho2000 <luis.ramost@pucp.edu.pe>
2023-06-07 22:26:01 -0500

    listo QR

--- src/main/resources/static/js/app.js
+++
@@ -1,7 +0,0 @@
-function getCurrentURL () {
-  return window.location.href
-}
-// Example
-const url = getCurrentURL()
-const QRcontenedor = document.getElementById('contenedorQR')
-new QRCode(QRcontenedor, url)
