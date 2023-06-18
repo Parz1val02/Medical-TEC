be9899190b2886f899d1fa88331f5b304c5eacfc
Luis Ramos <108200512+lucho2000@users.noreply.github.com>
2023-06-18 17:57:34 -0500

    dumps de la telesystem_2.1

---
+++ Dump20230618/telesystem_2_conversaciones.sql
@@ -0,0 +1,66 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: 35.231.147.174    Database: telesystem_2
+-- ------------------------------------------------------
+-- Server version	8.0.26-google
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '';
+
+--
+-- Table structure for table `conversaciones`
+--
+
+DROP TABLE IF EXISTS `conversaciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `conversaciones` (
+  `idconversaciones` int NOT NULL AUTO_INCREMENT,
+  `fechacreacion` datetime NOT NULL,
+  `usuario_dni` varchar(8) NOT NULL,
+  `usuario_dni1` varchar(8) NOT NULL,
+  PRIMARY KEY (`idconversaciones`),
+  KEY `fk_conversaciones_usuario1_idx` (`usuario_dni`),
+  KEY `fk_conversaciones_usuario2_idx` (`usuario_dni1`),
+  CONSTRAINT `fk_conversaciones_usuario1` FOREIGN KEY (`usuario_dni`) REFERENCES `usuario` (`dni`),
+  CONSTRAINT `fk_conversaciones_usuario2` FOREIGN KEY (`usuario_dni1`) REFERENCES `usuario` (`dni`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `conversaciones`
+--
+
+LOCK TABLES `conversaciones` WRITE;
+/*!40000 ALTER TABLE `conversaciones` DISABLE KEYS */;
+INSERT INTO `conversaciones` VALUES (1,'2023-04-22 20:00:00','12345678','14578934'),(2,'2023-04-22 16:00:00','14578934','17845767');
+/*!40000 ALTER TABLE `conversaciones` ENABLE KEYS */;
+UNLOCK TABLES;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-06-18 17:40:45
---
+++ Dump20230618/telesystem_2_mensajes.sql
@@ -0,0 +1,67 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: 35.231.147.174    Database: telesystem_2
+-- ------------------------------------------------------
+-- Server version	8.0.26-google
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '';
+
+--
+-- Table structure for table `mensajes`
+--
+
+DROP TABLE IF EXISTS `mensajes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `mensajes` (
+  `idmensajes` int NOT NULL AUTO_INCREMENT,
+  `contenido` varchar(500) NOT NULL,
+  `conversaciones_idconversaciones` int NOT NULL,
+  `fecha` date DEFAULT NULL,
+  `hora` time DEFAULT NULL,
+  `emisorDNI` varchar(8) DEFAULT NULL,
+  `receptorDNI` varchar(8) DEFAULT NULL,
+  PRIMARY KEY (`idmensajes`),
+  KEY `fk_mensajes_conversaciones1_idx` (`conversaciones_idconversaciones`),
+  CONSTRAINT `fk_mensajes_conversaciones1` FOREIGN KEY (`conversaciones_idconversaciones`) REFERENCES `conversaciones` (`idconversaciones`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mensajes`
+--
+
+LOCK TABLES `mensajes` WRITE;
+/*!40000 ALTER TABLE `mensajes` DISABLE KEYS */;
+INSERT INTO `mensajes` VALUES (1,'Le informamos que se ha generado una facturación (boleta) correspondiente a una cita previamente agendada.',1,'2023-05-22','20:00:00','22647853','12345678'),(2,'Le recordamos que su cita está programada para dentro de 10 minutos. Por favor, asegúrese de estar preparado y disponible para atender a su paciente puntualmente.',1,'2023-04-19','21:00:00','23456789','12345678'),(3,'Le informamos que se ha generado una facturación (boleta) correspondiente a una cita previamente agendada.',2,'2023-03-20','19:00:00','71231035','12345678');
+/*!40000 ALTER TABLE `mensajes` ENABLE KEYS */;
+UNLOCK TABLES;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-06-18 17:40:42
