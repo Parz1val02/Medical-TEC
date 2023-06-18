6ab0bf439c2b33796c96ac0da42b3ad72b3d5c48
CarlosPisco <edupiscote.cept@gmail.com>
2023-06-18 18:47:35 -0500

    autoregistro con nueva tabla y validaciones

---
+++ src/main/java/com/example/medicaltec/repository/FormAutoregistroRepository.java
@@ -0,0 +1,7 @@
+package com.example.medicaltec.repository;
+
+import com.example.medicaltec.Entity.FormAutoregistro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FormAutoregistroRepository extends JpaRepository<FormAutoregistro,Integer> {
+}
