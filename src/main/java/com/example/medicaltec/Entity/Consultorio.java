1592f53614bbaedf1b15b05b9cc52417dbc921b6
lucho2000 <luis.ramost@pucp.edu.pe>
2023-07-13 14:17:51 -0500

    cambio de entity de consultorio y ya estan todos los tipos de errores con vistas

--- src/main/java/com/example/medicaltec/Entity/Consultorio.java
+++ src/main/java/com/example/medicaltec/Entity/Consultorio.java
@@ -11,14 +11,17 @@ import lombok.Setter;
 public class Consultorio {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "idconsultorio", nullable = false, length = 5)
-    private String id;
+    @Column(name = "idconsultorio", nullable = false)
+    private Integer id;
 
     @ManyToOne
     @JoinColumn(name = "sedes_idsedes", nullable = false)
     private Sede sedesIdsedes;
 
     @ManyToOne
-    @JoinColumn(name = "horasdoctor_idhorasdoctor", nullable = false)
-    private Horasdoctor horasdoctor;
+    @JoinColumn(name = "dni")
+    private Usuario paciente_dni;
+
+    @Column(name = "nombreconsultorio", nullable = false, length = 5)
+    private String nombreConsultorio;
 }
